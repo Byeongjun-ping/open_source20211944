e0ccfc316e2d04087f404f2942130768f2487f48
Byeongjun-ping~ <minbyeongjun07@gmail.com>
2024-12-14 00:05:08 +0900

    원본 코드

--- 20211944.py
+++ 20211944.py
@@ -1,5 +1,25 @@
 ###################open_source_project_20211944###########
 
+#import time
+
+##타이머 시작점
+#start = input("Enter를 누르면 타이머를 시작합니다.")
+#begin = time.time()
+
+##타이머 종료점
+#stop = input("Enter를 누르면 측정을 종료합니다.")
+#end = time.time()
+
+##시간차
+#result = end - begin
+
+##여기서 round는 파이썬에서 소수점 자리수 조절에 활용됩니다.
+#result = round(result,3)
+#print("시작 후", result, "초의 시간이 흘렀습니다.")
+
+###################--{https://datazzang.tistory.com/6} -- 코드 출처
+
+
 import time
 from datetime import datetime
 
