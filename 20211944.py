27b2cd237fdf3ccdc8a03524953337dba9a0c1a0
Byeongjun-ping~ <minbyeongjun07@gmail.com>
2024-12-14 00:55:26 +0900

    code copy from github

--- 20211944.py
+++ 20211944.py
@@ -1,1 +1,16 @@
 ##########OpenSource_project###########
+
+#adj = input('Adjective: ')
+#verb1 = input('Verb: ')
+#verb2 = input('Verb: ')
+#noun1 = input('Noun: ')
+#place = input('Place: ')
+#noun2 = input('Noun: ')
+#name = input('Name: ')
+
+
+#madlib = f'Computer science is {adj}. I can {verb1} while sitting on a {noun2} #and {verb2}. I once went to the {place} and found a big {noun1}! I decided that would be a great place for {name}.'
+
+#print(madlib)
+
+###################################코드 카피#############################
