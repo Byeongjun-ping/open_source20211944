d1e5e1748919bc18bc091a205d64b64c0970c561
Byeongjun-ping~ <minbyeongjun07@gmail.com>
2024-12-14 01:01:02 +0900

    codecopy, 주석 입력

--- 20211944.py
+++ 20211944.py
@@ -1,5 +1,6 @@
 ##########OpenSource_project###########
 
+## 변수(각각 형용사, 동사1,2, 명사1,2, 장소, 이름 ) 에 키보드로 부터 입력받기 
 #adj = input('Adjective: ')
 #verb1 = input('Verb: ')
 #verb2 = input('Verb: ')
@@ -8,9 +9,11 @@
 #noun2 = input('Noun: ')
 #name = input('Name: ')
 
-
+## 매드립 변수에 f스트링 저장
+## f스트링에 문장과 앞서 입력한 단어 조합
 #madlib = f'Computer science is {adj}. I can {verb1} while sitting on a {noun2} #and {verb2}. I once went to the {place} and found a big {noun1}! I decided that would be a great place for {name}.'
 
+## 매드립 출력
 #print(madlib)
 
 ###################################코드 카피#############################
